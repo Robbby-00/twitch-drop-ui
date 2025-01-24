83d0d59e6134bb2788445be4eef6cd2ececbab18
Robbo <roberto.trinchini.02@gmail.com>
2025-01-24 18:41:06 +0100

    Hotfix v1.1.0

--- extra/start.bat
+++ extra/start.bat
@@ -3,16 +3,18 @@ set PATH=%~dp0\lib\;%PATH%
 
 if exist node_modules (
     call npm run update
+    if errorlevel 1 goto :modules
 ) else (
     echo "Skip update: no node_modules found!"
 )
 
+:modules
 if not exist node_modules (
     call npm install 
     call npx rebrowser-patches@latest patch --packageName puppeteer-core
-    if errorlevel 1 goto :next
+    if errorlevel 1 goto :run
 )
 
-:next
+:run
 call npm run production
 pause
